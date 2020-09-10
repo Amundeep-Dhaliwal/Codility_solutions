3ef71855f069c134170444e7aac43753a5ee855b
Amundeep Singh Dhaliwal <64365442+Amundeep-Dhaliwal@users.noreply.github.com>
2020-09-10 10:27:57 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -35,3 +35,21 @@ def solution(start,stop, step):
     else:
         return (stop - start) // step + 1
 
+# Permutation Missing Element
+def solution(A):
+    A = set(A)
+    missing = 1
+    while missing in A:
+        missing += 1
+    return missing
+
+# Tape Equilibrium
+def solution(A):
+    summation = sum(A)
+    m = float('inf')
+    left_sum = 0
+    for i in A[:-1]:
+        left_sum += i
+        m = min(abs(left_sum*2 - summation), m)
+    return m
+
