bcaaaa4e2c444056843640f2c2b8cebf4068c629
Amundeep Singh Dhaliwal <64365442+Amundeep-Dhaliwal@users.noreply.github.com>
2020-09-10 09:57:02 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -21,4 +21,17 @@ def solution(A, K):
     dlist.rotate(K)
     return list(dlist)
 
+# Odd Occurences In An Array
+def solution(A):
+    result = 0
+    for number in A:
+        result ^= number
+    return result
+
+# Frog Jump
+def solution(start,stop, step):
+    if (stop - start) % step == 0:
+        return (stop - start)//step
+    else:
+        return (stop - start) // step + 1
 
