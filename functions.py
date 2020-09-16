152e79450602dde65a25d350f2a15212857cf7fb
Amundeep Singh Dhaliwal <64365442+Amundeep-Dhaliwal@users.noreply.github.com>
2020-09-16 19:56:02 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -245,3 +245,18 @@ def solution(S):
         return 0
     else:
         return 1
+
+# Stone Wall
+def solution(H):
+    block_cnt = 0
+    stack = []
+    for height in H:
+        while len(stack) != 0 and stack[-1] > height:
+            stack.pop()
+        if len(stack) != 0 and stack[-1] == height:
+            pass
+        else:
+            block_cnt +=1 
+            stack.append(height)
+    return block_cnt
+
