5916e4b07eeef99743f97373a9e7e7768dcb3df3
Amundeep Singh Dhaliwal <64365442+Amundeep-Dhaliwal@users.noreply.github.com>
2020-09-17 13:16:38 +0100

    Update leader.py
    Please see a description for the equilibrium leader here: https://curt-park.github.io/2018-09-06/algorithm-equileader/

--- leader.py
+++ leader.py
@@ -10,3 +10,22 @@ def solution(A):
         return A.index(mostCommon[0])
     else:
         return -1
+    
+# Equilibrium Leader
+from collections import defaultdict
+def solution(A):
+    markerLeft = defaultdict(lambda : 0)
+    markerRight = defaultdict(lambda : 0)
+    for i in range(len(A)):
+        markerRight[A[i]]+=1
+    numberLeaders= 0
+    leader =A[0]
+    for i in range(len(A)):
+        markerRight[A[i]]-=1
+        markerLeft[A[i]]+=1
+        if i== 0 or markerLeft[leader]<markerLeft[A[i]]:
+            leader= A[i]
+        
+        if (i+1)//2 <markerLeft[leader] and (len(A) -(i+1))//2<markerRight[leader]:
+            numberLeaders+= 1
+    return numberLeaders
