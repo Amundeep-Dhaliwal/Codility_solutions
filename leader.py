918c77c707e74e0d702b353477bd519d00bd02a6
Amundeep Singh Dhaliwal <64365442+Amundeep-Dhaliwal@users.noreply.github.com>
2020-09-17 11:45:59 +0100

    Update leader.py

--- leader.py
+++ leader.py
@@ -1,1 +1,12 @@
 # Dominator
+from collections import Counter
+def solution(A):
+    length = len(A)
+    if length < 1:
+        return -1
+    countDict = Counter(A)
+    mostCommon = countDict.most_common()[0]
+    if mostCommon[1] > int(length/2):
+        return A.index(mostCommon[0])
+    else:
+        return -1
